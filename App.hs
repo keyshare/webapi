2d932f40de19b6227c0cc70c49ab6ba59838c100
pylover <vahid.mardani@gmail.com>
2024-02-05 11:51:52 +0330

    Simple routing

--- App.hs
+++ App.hs
@@ -4,14 +4,36 @@ module App (
     ) where
 
 
+import Data.Text
+import Data.ByteString.Lazy.Internal (ByteString)
 import Network.Wai
 import Network.HTTP.Types
 
 
+okText :: ByteString -> (Response -> IO ResponseReceived) -> IO ResponseReceived
+okText body respond = respond $ responseLBS 
+  status200 
+  [("Content-Type", "text/plain")] 
+  (body <> "\r\n")
+
+
+foo :: Application
+foo _ = okText "Hello, foo!"
+
+
+foobar :: Application
+foobar _ = okText "Hello, foo bar!"
+
+
+root :: Application
+root _ = okText "Hello, root!"
+
+
+route :: [Text] -> Application
+route ("foo":[]) = foo
+route ("foo":"bar":_) = foobar
+route _ = root
+
+
 keyshare :: Application
-keyshare _ respond = do
-    putStrLn "I've done some IO here"
-    respond $ responseLBS
-        status200
-        [("Content-Type", "text/plain")]
-        "Hello, Web!"
+keyshare req = route (pathInfo req) req
